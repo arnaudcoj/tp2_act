aded2e0e4c5ef554ad26355bae68fc17e5e5cde2
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2015-10-04 19:01:58 +0200

    added Triplet class

---
+++ Triplet.java
@@ -0,0 +1,27 @@
+
+public class Triplet
+{
+    private int a;
+    private int b;
+    private int c;
+
+    Triplet(int a, int b, int c)
+    {
+	this.a = a;
+	this.b = b;
+	this.c = c;
+    }
+
+    int getA() {
+	return a;
+    }
+    
+    int getB() {
+	return b;
+    }
+    
+    int getC() {
+	return c;
+    }
+    
+}
